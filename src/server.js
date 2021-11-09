ee004253b3d261e94fd42ac90392d96f4efe22d4
Bizeun <68911658+Bizeun@users.noreply.github.com>
2021-11-08 22:56:23 -0800

    my first server
    practice to use request and response

--- src/server.js
+++ src/server.js
@@ -1,9 +1,28 @@
 import express from "express";
 
-const PORT = 5050;
+const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res)=>{
+    return res.send("<h1>Hello</h1>");
+};
+const handleAbout = (req, res)=>{
+    return res.send("<h1>How about</h1>");
+};
+const handleContact = (req, res)=>{
+    return res.send("<h1>Contact</h1>");
+};
+const handleLogin = (req, res)=>{
+    return res.send("<h1>Login in here</h1>");
+};
+
+app.get("/", handleHome);
+app.get("/about",handleAbout);
+app.get("/contact",handleContact);
+app.get("/login",handleLogin);
+
+
 const handleListening = () => console.log (`Server on port http://localhost:${PORT}`);
 
-app.listen(5050, handleListening);
+app.listen(4000, handleListening);
