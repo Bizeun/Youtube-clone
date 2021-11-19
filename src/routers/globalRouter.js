69110b8a2ca923dc9d4fb0565fc8d26a6318c2d6
Bizeun <68911658+Bizeun@users.noreply.github.com>
2021-11-19 01:17:31 -0800

    To make pages
    practice for making pages and connecting MongoDB server

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,10 @@
 import express from "express";
-import {home, join, login} from "../controllers/userController";
-import {trending, somethingNew} from "../controllers/storyController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", home);
-globalRouter.get("/trending", trending);
-globalRouter.get("/new", somethingNew);
+globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import storyRouter from "./routers/storyRouter";
+import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/",globalRouter);
-app.use("/stories",storyRouter);
-app.use("/users",userRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log (`Server on port http://localhost:${PORT}`);
 
