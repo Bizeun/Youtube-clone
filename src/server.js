657cd6949b99151ea67816583e348fe10d822f88
Bizeun <68911658+Bizeun@users.noreply.github.com>
2021-11-22 03:07:58 -0800

    To connect with DB
    practice for connecting with db

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
 
@@ -18,6 +16,4 @@ app.use("/",globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log (`Server on port http://localhost:${PORT}`);
-
-app.listen(4000, handleListening);
+export default app;
